63da1d9041293768bc048e3060f1694b03bb6b2e
Fabio Simoes <59034232+fabioes99@users.noreply.github.com>
2023-07-20 12:06:44 -0300

    Update cria_usuarios.sh
    adicionando readme

--- cria_usuarios.sh
+++ cria_usuarios.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-rm -Rf /adm
-rm -Rf /public
-rm -Rf /mariana
-rm -Rf /teste
-rm -Rf /textos
-rm -Rf /ven
-
-userdel -r maisa
-userdel -r daniel
-userdel -r debora
-userdel -r teste
-userdel -r rodrigo
-userdel -r adm
-userdel -r guest
-userdel -r guest1
-userdel -r guest2
-userdel -r guest3
-userdel -r joao
-
-groupdel GRP_ADM
-groupdel GRP_VEN
-
 echo "Criando publico"
 
 mkdir /publico
