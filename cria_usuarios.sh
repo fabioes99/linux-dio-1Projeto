ca304c67a54b40faa9b90ec4a01fc1360c01168c
Fabio Simoes <59034232+fabioes99@users.noreply.github.com>
2023-07-20 11:59:21 -0300

    Update cria_usuarios.sh
    atualizações nos usuarios

--- cria_usuarios.sh
+++ cria_usuarios.sh
@@ -33,30 +33,31 @@ groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-useradd -m -G GRP_ADM carlos
-useradd -m -G GRP_ADM maria
-useradd -m -G GRP_ADM joao
+useradd -m -s /bin/bash -p $(openssl passwd -6 senha123) -G GRP_ADM carlos 
+useradd -m -s /bin/bash -p $(openssl passwd -6 senha123) -G GRP_ADM maria
+useradd -m -s /bin/bash -p $(openssl passwd -6 senha123) -G GRP_ADM joao
 
 passwd carlos
 passwd maria
 passwd joao
 
-useradd -m -G GRP_VEN debora
-useradd -m -G GRP_VEN sebastiana
-useradd -m -G GRP_VEN roberto
+useradd -m -s /bin/bash -p $(openssl passwd -6 senha123)  -G GRP_VEN debora
+useradd -m -s /bin/bash -p $(openssl passwd -6 senha123)  -G GRP_VEN sebastiana
+useradd -m -s /bin/bash -p $(openssl passwd -6 senha123)  -G GRP_VEN roberto
 
 passwd debora
 passwd sebastiana
 passwd roberto
 
-useradd -m -G GRP_SEC josefina
-useradd -m -G GRP_SEC amanda
-useradd -m -G GRP_SEC rogerio
+useradd -m -s /bin/bash -p $(openssl passwd -6 senha123)  -G GRP_SEC josefina
+useradd -m -s /bin/bash -p $(openssl passwd -6 senha123)  -G GRP_SEC amanda
+useradd -m -s /bin/bash -p $(openssl passwd -6 senha123)  -G GRP_SEC rogerio
 
 passwd josefina
 passwd amanda
 passwd rogerio
 
+echo "criando pastas e permissoes de grupos"
 
 mkdir /adm
 chown root:GRP_ADM /adm
@@ -70,5 +71,5 @@ mkdir /sec
 chown root:GRP_SEC /sec
 chmod 770 /sec
 
-
+echo "fim do script"
 
